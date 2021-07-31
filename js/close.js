ef834fbb65d796dcd4a88cc2623f889a8f27fcdc
rezaprtma <novianrezapratama@gmail.com>
2021-07-31 14:11:06 +0700

    memperbarui tampilan

--- js/close.js
+++ js/close.js
@@ -1,12 +1,12 @@
-// menutub tab
-function countdown() {
-  var i = document.getElementById("counter");
-  i.innerHTML = parseInt(i.innerHTML)-1;
-  if (parseInt(i.innerHTML) <= 0) {
-    window.close();
-  }
-}
+// // menutub tab
+// function countdown() {
+//   var i = document.getElementById("counter");
+//   i.innerHTML = parseInt(i.innerHTML)-1;
+//   if (parseInt(i.innerHTML) <= 0) {
+//     window.close();
+//   }
+// }
 
-setInterval(function () {
-  countdown();
-}, 1000);
+// setInterval(function () {
+//   countdown();
+// }, 1000);
