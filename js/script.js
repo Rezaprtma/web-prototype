475b6a3b34f221fe9b3b47814a99c33869dbf265
rezaprtma <novianrezapratama@gmail.com>
2021-09-12 12:38:18 +0700

    menambahkan loading

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
-// // Pertanyaan
-// var pertanyaan = prompt(" 1 + 1 = ...");
-// var password = "2";
 
-// if (pertanyaan == password) {
-//   ("");
-// } else {
-//   close();
-// }
 
 // Link Instagram
 var instagram = document.getElementById("instagram").addEventListener("click", tampilkanAlertInstagram);
 function tampilkanAlertInstagram() {
   var tes = confirm("Anda yakin ingin mengunjungi instagram?");
   if (tes) {
-    window.open("https://instagram.com/__diamondlight__", "_blank");
+    window.open("https://instagram.com/pendiam__", "_blank");
   } else {
     getElementById("home");
   }
-  return;
 }
 
 // Link Github
@@ -25,9 +16,9 @@ var instagram = document.getElementById("github").addEventListener("click", tamp
 function tampilkanAlertGithub() {
   var tes = confirm("Anda yakin ingin mengunjungi github?");
   if (tes) {
-    window.open("https://github.com/Rezaprtma/Rezaprtma.github.io ", "_blank");
+    window.open("https://github.com/Rezaprtma", "_blank");
   } else {
     getElementById("home");
   }
-  return;
 }
+
